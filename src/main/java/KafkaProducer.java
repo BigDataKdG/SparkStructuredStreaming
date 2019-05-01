68495c7b064ffd6cb3706e511ed4441cc38270af
Jens Bosman <jens_bosman@hotmail.com>
2019-05-01 16:08:11 +0200

    add loop

--- src/main/java/KafkaProducer.java
+++ src/main/java/KafkaProducer.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -17,13 +18,11 @@ import model.ContainerMelding;
 public class KafkaProducer {
     private final static String TOPIC = "test";
     private final static String BOOTSTRAP_SERVER = "localhost:9092";
+    private final static String jdbcUrl = "jdbc:postgresql://localhost:5432/sorteertstraatjes";
+    private final static String username = "postgres";
+    private final static String password = "admin";
 
     public static void main(String[] args) throws Exception {
-        String jdbcUrl = "jdbc:postgresql://localhost:5432/sorteertstraatjes";
-        String username = "postgres";
-        String password = "admin";
-
-
         Properties props = new Properties();
         props.put("bootstrap.servers", BOOTSTRAP_SERVER);
         props.put("group.id", "test");
@@ -32,16 +31,26 @@ public class KafkaProducer {
 
         Producer<Integer, ContainerMelding> kafkaProducer =
                 new org.apache.kafka.clients.producer.KafkaProducer(props);
+        int counter = 0;
+        while (true) {
+            sendToKafka(retrieveMeldingenFromDatabase(counter), kafkaProducer);
+            Thread.sleep(10000);
+            counter += 50;
+        }
+    }
 
+    private static List<ContainerMelding> retrieveMeldingenFromDatabase(int counter) throws SQLException {
         List<ContainerMelding> meldingenList = new ArrayList<>();
+        String sql =
+                "SELECT * FROM public.container LIMIT ? OFFSET ?";
+        Connection con = DriverManager.getConnection(jdbcUrl, username, password);
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1, 50);
+        st.setInt(2, counter);
+        ResultSet rs = st.executeQuery();
 
-        try (Connection con = DriverManager.getConnection(jdbcUrl, username, password);
-             Statement st = con.createStatement();
-
-             ResultSet rs = st.executeQuery("SELECT * FROM \"public\".\"container\" limit 100")) {
-
-            if (rs.next()) {
-                System.out.println(rs.getString("datum_tijdstip_containeractiviteit"));
+        try {
+            while (rs.next()) {
                 meldingenList.add(ContainerMelding.builder()
                         .containerActiviteit(rs.getDate("datum_tijdstip_containeractiviteit").toLocalDate())
                         .containerNummer(rs.getInt("container_nr"))
@@ -49,13 +58,18 @@ public class KafkaProducer {
                         .containerMeldingCategorie(rs.getString("containermelding_categorie_code"))
                         .build());
             }
-        } catch (SQLException ex) {
+        } catch (Exception ex) {
 
         }
-        for (ContainerMelding melding : meldingenList) {
-            kafkaProducer.send(new ProducerRecord(TOPIC, 0, melding));
-        }
-        kafkaProducer.close();
+        return meldingenList;
     }
 
+    private static void sendToKafka(List<ContainerMelding> meldingenList,
+                                    final Producer<Integer, ContainerMelding> kafkaProducer) {
+        for (ContainerMelding melding : meldingenList) {
+            System.out.println(melding);
+            kafkaProducer.send(new ProducerRecord(TOPIC, 0, melding));
+
+        }
+    }
 }
